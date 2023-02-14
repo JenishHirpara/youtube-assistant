775272c7b3f86e25d77989a8e20bbcb7fba51a4e
Jenish Hirpara <jenishhirpara23@gmail.com>
2023-02-13 23:37:03 -0600

    minor commit

--- routes/keyword.js
+++ routes/keyword.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {testing} = require('../controller/keyword')
-router.route('/:Id').get(testing);
+const {testing,getVideo,getTopResults,insertVideo,insertKey,getRedditData} = require('../controller/keyword')
+router.route('/:Id').get(getVideo).post(insertVideo);
+router.route('/key/:Id').post(insertKey).get(getTopResults);
+router.route('/entity/redditData').post(getRedditData)
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const errorHandle = require('./middleware/error');
 const connectDb = require('./config/db');
+const cors = require
 dotenv.config({path: './config/config.env'});
 const app = express();
 const PORT = process.env.PORT;
@@ -10,6 +11,12 @@ connectDb();
 
 app.use(express.json());
 const keyword = require('./routes/keyword');
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(errorHandle);
 app.use('/api/v1/keyword',keyword);
